f085144574bc3af3048dd181e239a5fc72d8dc0c
Juan Pablo Rey <juanpablorey@restorando.com>
2016-03-28 15:08:40 -0300

    Update Checkstyle version Set up Travis

--- src/main/java/gps/GPSNode.java
+++ src/main/java/gps/GPSNode.java
@@ -4,60 +4,78 @@ import gps.api.GPSState;
 
 public class GPSNode {
 
-	private GPSState state;
+  private GPSState state;
+  private GPSNode parent;
+  private Integer cost;
 
-	private GPSNode parent;
+  public GPSNode(GPSState state, Integer cost) {
+    this.state = state;
+    this.cost = cost;
+  }
 
-	private Integer cost;
+  public GPSNode getParent() {
+    return parent;
+  }
 
-	public GPSNode(GPSState state, Integer cost) {
-		this.state = state;
-		this.cost = cost;
-	}
+  public void setParent(GPSNode parent) {
+    this.parent = parent;
+  }
 
-	public GPSNode getParent() {
-		return parent;
-	}
+  public GPSState getState() {
+    return state;
+  }
 
-	public void setParent(GPSNode parent) {
-		this.parent = parent;
-	}
+  public Integer getCost() {
+    return cost;
+  }
 
-	public GPSState getState() {
-		return state;
-	}
+  @Override
+  public String toString() {
+    return state.toString();
+  }
 
-	public Integer getCost() {
-		return cost;
-	}
+  public String getSolution() {
+    if (this.parent == null) {
+      return this.state.toString();
+    }
+    return this.parent.getSolution() + this.state.toString();
+  }
 
-	@Override
-	public String toString() {
-		return state.toString();
-	}
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((cost == null) ? 0 : cost.hashCode());
+    result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+    result = prime * result + ((state == null) ? 0 : state.hashCode());
+    return result;
+  }
 
-	public String getSolution() {
-		if (this.parent == null) {
-			return this.state.toString();
-		}
-		return this.parent.getSolution() + this.state.toString();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GPSNode other = (GPSNode) obj;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		return true;
-	}
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    GPSNode other = (GPSNode) obj;
+    if (cost == null) {
+      if (other.cost != null)
+        return false;
+    } else if (!cost.equals(other.cost))
+      return false;
+    if (parent == null) {
+      if (other.parent != null)
+        return false;
+    } else if (!parent.equals(other.parent))
+      return false;
+    if (state == null) {
+      if (other.state != null)
+        return false;
+    } else if (!state.equals(other.state))
+      return false;
+    return true;
+  }
 
 }
