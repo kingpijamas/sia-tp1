dadfc8c8d1aac652729b6c297793ff5eea2a5f51
Santiago  Camisay <scamisay@localhost.localdomain>
2016-04-04 13:07:19 -0300

    Add idea dependency

--- src/main/java/ar/itba/edu/sia/tp1/calcudoku/CalcudokuRule.java
+++ src/main/java/ar/itba/edu/sia/tp1/calcudoku/CalcudokuRule.java
@@ -1,42 +1,60 @@
 package ar.itba.edu.sia.tp1.calcudoku;
 
 import static ar.itba.edu.sia.tp1.utils.ObjectUtils.toStringBuilder;
+
 import ar.itba.edu.sia.tp1.calcudoku.domain.Position;
 import ar.itba.edu.sia.tp1.gps.GPSRule;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by scamisay on 02/04/16.
  */
 public class CalcudokuRule implements GPSRule {
-	private final Position position;
-	private final int value;
+    private final Set<Position> positions = new HashSet<>();
 
-	public CalcudokuRule(Position position, int value) {
-		this.position = position;
-		this.value = value;
-	}
+    public CalcudokuRule(Position pos1, Position pos2) {
+        positions.add(pos1);
+        positions.add(pos2);
+    }
 
-	public Position getPosition() {
-		return position;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CalcudokuRule)) return false;
 
-	public int getValue() {
-		return value;
-	}
+        CalcudokuRule that = (CalcudokuRule) o;
 
-	@Override
-	public Integer getCost() {
-		return null; // TODO
-	}
+        return positions.equals(that.positions);
+    }
 
-	@Override
-	public String getName() {
-		return null; // TODO
-	}
+    @Override
+    public int hashCode() {
+        return positions.hashCode();
+    }
 
-	@Override
-	public String toString() {
-		return toStringBuilder(this).append("position", position)
-				.append("value", value).toString();
-	}
+    @Override
+    public Integer getCost() {
+        return null; // TODO
+    }
+
+    @Override
+    public String getName() {
+        return null; // TODO
+    }
+
+    @Override
+    public String toString() {
+        return toStringBuilder(this).append("position", position)
+                .append("value", value).toString();
+    }
+
+    public Position getFrom() {
+        return from;
+    }
+
+    public Position getTo() {
+        return to;
+    }
 }
