2fb9d322082ac2eee497e70fdef1c2f62cf5fba3
administrator <fernando.en.caballito@gmail.com>
2016-04-07 14:50:58 -0300

    testing

--- src/main/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/H2.java
+++ src/main/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/H2.java
@@ -4,7 +4,7 @@ public class H2 extends Heuristic{
 
 	@Override
 	int getH(int incorrect_groups, int incorrect_columns) {
-		return (int) Math.ceil((incorrect_columns+incorrect_groups)/4);
+		return (int) Math.ceil((incorrect_columns+incorrect_groups)/4.0);
 	}
 
 }
--- src/test/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/HeuristicsTest.java
+++ src/test/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/HeuristicsTest.java
@@ -1,22 +1,63 @@
 package ar.itba.edu.sia.tp1.calcudoku.heuristics;
 
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
 import java.util.List;
 
 import org.junit.Test;
 
 import ar.itba.edu.sia.tp1.calcudoku.domain.Board;
 import ar.itba.edu.sia.tp1.calcudoku.domain.Group;
+import ar.itba.edu.sia.tp1.calcudoku.domain.Operator;
+import ar.itba.edu.sia.tp1.calcudoku.domain.Position;
 
 public class HeuristicsTest {
 
 	
 	@Test
 	public void test1(){
-		List<Group> groups=null;
+		List<Position> positions1 = asList(position(0, 0), position(0, 1)
+				);
+		Group group1=new Group(positions1, Operator.PLUS, 3);
+		
+		
+		List<Position> positions2 = asList(position(1, 0));
+		Group group2=new Group(positions2, Operator.IDENTITY,2);
+		
+		List<Position> positions3 = asList(position(1, 1));
+		Group group3=new Group(positions3, Operator.IDENTITY,1);
+		
+		
+			
+		List<Group> groups = asList(group1,group2,group3);
+		
+		
+		
+		
 		Board board=new Board(2, groups);
+		
+		board.put(position(0,0), 2);
+		board.put(position(0,1), 1);
+		board.put(position(1, 0), 1);
+		board.put(position(1, 1), 2);
+		
+		int invalid_columns=board.invalidColumnsCount();
+		int invalid_groups=board.invalidGroupsCount();
+		
+		
+		assertEquals(0,invalid_columns);
+		
+		assertEquals(2,invalid_groups);
+		H1 h1=new H1();
+		assertEquals(1,h1.getH(board));
+		H2 h2=new H2();
+		assertEquals(1,h2.getH(board));
 		
 		
 	}
-	
+	private Position position(int row, int col) {
+		return new Position(row, col);
+	}
 
 }
