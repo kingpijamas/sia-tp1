fc65f5a1b040b8e7d85f43a0e1753df355740f6d
Fernando  Bejerano Gonzalez <fbejaran@localhost.localdomain>
2016-04-08 14:29:07 -0300

    cambio menor

--- src/main/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/Heuristic.java
+++ src/main/java/ar/itba/edu/sia/tp1/calcudoku/heuristics/Heuristic.java
@@ -1,9 +1,22 @@
 package ar.itba.edu.sia.tp1.calcudoku.heuristics;
 
+import ar.itba.edu.sia.tp1.calcudoku.CalcudokuState;
 import ar.itba.edu.sia.tp1.calcudoku.domain.Board;
+import ar.itba.edu.sia.tp1.gps.GPSHeuristic;
 
-public abstract class Heuristic {
+public abstract class Heuristic implements GPSHeuristic<CalcudokuState>{
 
+	public int getValue(CalcudokuState state){
+		Board board=state.getBoard();
+		return getH(board);
+	}
+	
+	public int getH(CalcudokuState state){
+		Board board=state.getBoard();
+		return getH(board);
+		
+		
+	}
 	
 	public int getH(Board board){
 		int incorrect_groups=board.invalidGroupsCount();
